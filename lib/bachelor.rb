f9fccc43af57cb38ff473ac0aba9b84cdcf34843
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-31 13:53:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -84,7 +84,7 @@ def get_average_age_for_season(data, season)
       #}
       #binding.pry
       average_age = new_array.inject{ |sum, el| sum + el }.to_f / new_array.length
-     
+     return average_age
     }
   end
   } 
